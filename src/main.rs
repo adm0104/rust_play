0531558fb3b7f5afc785bd3f699a06d92bf44e0b
Andrew Malone <amalone37@gatech.edu>
2024-03-16 05:36:06 +0000

    more learning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let something: Vec<String> = env::args().collect();
+    for (index, argument) in args.iter().enumerate().skip(1) {
+        println!("Argument {}: {}", index, argument);
+    }
+    for (index, argument) in something.iter().enumerate().skip(1) {
+        println!("Argument {}: {}", index, argument);
+    }
 }
